2284fdbeff64c4423a93c58f6cf8a667a3a1cc59
natsu242 <romimboumba7@gmail.com>
2026-06-17 15:14:46 +0100

    feat: add config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  BOT_NAME: "DENTSUS V7 XMD",
+  VERSION: "7.0.0",
+
+  PREFIX: ".",
+
+  OWNER: ["242053323191", "242065121108"],
+  OWNER_NAME: "Natsu Tech / Dev <>",
+
+  PACK_NAME: "DENTSU XMD Stickers",
+  AUTHOR: "Natsu Tech",
+
+  SESSION_ID: process.env.SESSION_ID || "",
+
+  AUTOREAD_ENABLED:   process.env.AUTOREAD_ENABLED   === "true",
+  AUTOTYPING_ENABLED: process.env.AUTOTYPING_ENABLED === "true",
+  AUTORECORD_ENABLED: process.env.AUTORECORD_ENABLED === "true",
+  AUTOLIKE_ENABLED:   process.env.AUTOLIKE_ENABLED   === "true",
+};
